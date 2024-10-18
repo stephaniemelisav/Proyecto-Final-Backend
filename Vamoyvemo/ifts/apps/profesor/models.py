600f7ba2f5c8e5a2225e58b028343e2c8260279a
ozunacristian <ozunacristianj@gmail.com>
2024-10-18 20:22:37 -0300

    Se añade atributos a modelo profesor.

--- Vamoyvemo/ifts/apps/profesor/models.py
+++ Vamoyvemo/ifts/apps/profesor/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class Profesor(models.Model):
     nombre = models.CharField(max_length=255)
+    especialidad = models.CharField(max_length=100)
+
+def __str__(self):
+    return f"{self.nombre} {self.especialidad}"
     
