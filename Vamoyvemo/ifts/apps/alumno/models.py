f679b6e3f4be32d48bc557afc9c4de70d3c2a2e1
ozunacristian <ozunacristianj@gmail.com>
2024-10-18 18:00:56 -0300

    Se añade atributos a modelo alumno.

--- Vamoyvemo/ifts/apps/alumno/models.py
+++ Vamoyvemo/ifts/apps/alumno/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Alumno(models.Model):
     nombre = models.CharField(max_length=100)
-    #anios = models.CharField(max_length=50)
-    descripcion = models.IntegerField()
+    matricula = models.CharField(max_length=20)
+    fecha_ingreso = models.DateField()
+    estado_academico = models.BooleanField()
+
     
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} {self.matricula} {self.fecha_ingreso} {self.estado_academico}"
         
