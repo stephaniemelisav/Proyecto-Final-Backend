e99250ea212ee85be9292f8ad6592e2382f8acd3
ozunacristian <ozunacristianj@gmail.com>
2024-10-22 17:48:51 -0300

    se modifican apps.py de todos los modelos debido al directorio apps.

--- Vamoyvemo/ifts/apps/inscripcion/apps.py
+++ Vamoyvemo/ifts/apps/inscripcion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InscripcionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inscripcion'
+    name = 'apps.inscripcion'
