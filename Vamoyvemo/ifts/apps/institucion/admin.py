0f4f855996c78051f13cb303c7978a4aecbf984f
ozunacristian <ozunacristianj@gmail.com>
2024-11-07 23:06:35 -0300

    Se elimina aplicación institucion.

--- Vamoyvemo/ifts/apps/institucion/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from apps.institucion.models  import Institucion
-
-# Register your models here.
-@admin.register(Institucion)
-class InstitucionAdmin(admin.ModelAdmin):
-  list_display = ('nombre',)
--- Vamoyvemo/ifts/apps/institucion/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Institucion(models.Model):
-  nombre = models.CharField(max_length=50)
-
-  def __str__(self):
-    return f"{self.nombre}"
