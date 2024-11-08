336424875a68636c0da53ae19a1c8aa5c50329e3
ozunacristian <ozunacristianj@gmail.com>
2024-11-07 23:05:29 -0300

    Se elimina aplicación inscripcion.

--- Vamoyvemo/ifts/apps/inscripcion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.inscripcion.models import Inscripcion
-
-@admin.register(Inscripcion)
-class InscripcionAdmin(admin.ModelAdmin):
-  list_display = ('fecha_inscripcion', 'estado', 'cursada')
--- Vamoyvemo/ifts/apps/inscripcion/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Inscripcion(models.Model):
-  fecha_inscripcion = models.DateField()
-  estado = models.BooleanField()
-  cursada = models.OneToOneField('cursada.Cursada', on_delete=models.CASCADE, related_name='inscripciones', default=None)
-
-  def __str__(self):
-    return f"{self.fecha_inscripcion} {self.estado} {self.cursada}"
