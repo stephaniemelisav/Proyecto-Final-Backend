c1a3554d81e97bdbe32e4a76e52b618cd03c7300
ozunacristian <ozunacristianj@gmail.com>
2024-11-16 14:48:39 -0300

    modelado y registro de app Materia

--- Vamoyvemo/ifts/apps/materia/admin.py
+++ Vamoyvemo/ifts/apps/materia/admin.py
@@ -4,8 +4,4 @@ from apps.materia.models import Materia, MateriaProfesor
 
 @admin.register(Materia)
 class MateriaAdmin(admin.ModelAdmin):
-  list_display = ('nombre', 'descripcion')
-
-@admin.register(MateriaProfesor)
-class ProfesorAdmin(admin.ModelAdmin):
-  list_display = ('fecha_asignacion',)
+  list_display = ('codigo', 'nombre', 'descripcion')
--- Vamoyvemo/ifts/apps/materia/models.py
+++ Vamoyvemo/ifts/apps/materia/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
 class Materia(models.Model):
+  codigo = models.CharField(max_length=20)
   nombre = models.CharField(max_length=80)
   descripcion = models.CharField(max_length=150)
 
   def __str__(self):
-    return f"{self.nombre} {self.descripcion}"
-
-class MateriaProfesor(models.Model):
-  materia = models.ForeignKey("materia.Materia", on_delete=models.CASCADE, related_name="materias")
-  profesor = models.ForeignKey("profesor.Profesor", on_delete=models.CASCADE, related_name="profesores")
-  fecha_asignacion = models.DateField()
-
-  def __str__(self):
-    return f"{self.fecha_asignacion}"
+    return f"{self.codigo} {self.nombre} {self.descripcion}"
