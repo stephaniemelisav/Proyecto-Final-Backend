1abd56158746b487c43ac67255f5934753cffbba
ozunacristian <ozunacristianj@gmail.com>
2024-11-16 14:34:09 -0300

    modelado y registro en admin de cicloLectivo

--- Vamoyvemo/ifts/apps/cicloLectivo/admin.py
+++ Vamoyvemo/ifts/apps/cicloLectivo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.cicloLectivo.models import CicloLectivo
 
-# Register your models here.
+@admin.register(CicloLectivo)
+class CicloLectivoAdmin(admin.ModelAdmin):
+  list_display = ['anio', 'periodo', 'fecha_inicio', 'fecha_fin', 'estado']
+
--- Vamoyvemo/ifts/apps/cicloLectivo/models.py
+++ Vamoyvemo/ifts/apps/cicloLectivo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class CicloLectivo(models):
+  anio = models.IntegerField()
+  periodo = models.CharField(max_length=50)
+  fecha_inicio = models.DateField()
+  fecha_fin = models.DateField()
+  estado = models.BooleanField()
+
+  def __str__(self):
+    return f"{self.anio} {self.periodo} {self.fecha_inicio} {self.fecha_fin} {self.estado}"
