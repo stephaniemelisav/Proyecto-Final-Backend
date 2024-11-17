9b612d18c670c2815b34f376f19cf173d134850e
ozunacristian <ozunacristianj@gmail.com>
2024-11-17 00:12:04 -0300

    fix: importación de módulo inválida.

--- Vamoyvemo/ifts/apps/materia/admin.py
+++ Vamoyvemo/ifts/apps/materia/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.materia.models import Materia, MateriaProfesor
+from apps.materia.models import Materia
 
 @admin.register(Materia)
 class MateriaAdmin(admin.ModelAdmin):
