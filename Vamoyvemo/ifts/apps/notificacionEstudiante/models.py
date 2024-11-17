27b1b2c3c891748271a7b6b846d1f58f01441248
ozunacristian <ozunacristianj@gmail.com>
2024-11-16 21:56:05 -0300

    modelado y registro en panel de admin para app notificacion.

--- Vamoyvemo/ifts/apps/notificacionEstudiante/models.py
+++ Vamoyvemo/ifts/apps/notificacionEstudiante/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class NotificacionEstudiante(models.Model):
+  evento = models.ForeignKey('evento.Evento', on_delete=models.CASCADE, related_name='notificacionesEstudiantes')
+  alumno = models.ForeignKey('alumno.Alumno', on_delete=models.CASCADE, related_name='notificacionesEstudiantes')
+  leido = models.BooleanField(default=False)
+  calificacion = models.IntegerField()
+
+  def __str__(self):
+    return f"{self.evento} {self.alumno} {self.leido} {self.calificacion}"
