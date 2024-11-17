0fba5d1d0ebcff5d3cbaa0e4e46b1d719533ac82
Steph <stephaniemelisav@gmail.com>
2024-11-16 21:24:54 -0300

    Configuración de BS postgres

--- Vamoyvemo/ifts/siukiller/settings.py
+++ Vamoyvemo/ifts/siukiller/settings.py
@@ -94,9 +94,13 @@ WSGI_APPLICATION = 'siukiller.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_backend",
+        "USER": "soporte",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
