7bceeb43423d485ba20ce848573be1e26ff17330
ozunacristian <ozunacristianj@gmail.com>
2024-11-17 00:22:33 -0300

    fix: errores de tipeo y omisión.

--- Vamoyvemo/ifts/apps/cursada/models.py
+++ Vamoyvemo/ifts/apps/cursada/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Cursada(models.Model):
-  alumno = models.ForeignKey('alumno.Alumno', on_delete=models.CASCADE, related_name='alumnos')
-  ciclo_lectivo = models.ForeignKey('cicloLectivo.CicloLectivo', on_delete=models.CASCADE, related_name='ciclosLectivos')
-  materia = models.ForeignKey('materia.Materia', on_delete=models.CASCADE, related_name='materias')
+  alumno = models.ForeignKey('alumno.Alumno', on_delete=models.CASCADE, related_name='cursadas')
+  ciclo_lectivo = models.ForeignKey('cicloLectivo.CicloLectivo', on_delete=models.CASCADE, related_name='cursadas')
+  materia = models.ForeignKey('materia.Materia', on_delete=models.CASCADE, related_name='cursadas')
 
   estado = models.BooleanField()
   fecha_inscripcion = models.DateField()
--- Vamoyvemo/ifts/apps/profesor/models.py
+++ Vamoyvemo/ifts/apps/profesor/models.py
@@ -6,7 +6,7 @@ class Profesor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
-    telefono = models.CharField(20)
+    telefono = models.CharField(max_length=20)
 
 def __str__(self):
     return f"{self.legajo} {self.nombre} {self.apellido} {self.email} {self.telefono}"
